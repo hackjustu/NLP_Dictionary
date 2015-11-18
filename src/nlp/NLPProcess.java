90d1d03782fb60557869e5f9f12e91b73ef0d27d
hackjustu <registercosmo@gmail.com>
2015-11-17 22:02:32 -0800

    add commetns

--- src/nlp/NLPProcess.java
+++ src/nlp/NLPProcess.java
@@ -10,23 +10,29 @@ public class NLPProcess {
 
         TreeSet<String> dict = new TreeSet<>();
 
+        // Read the raw data from the resource folder
         try (BufferedReader br = new BufferedReader(new FileReader("resource/OANC_written_all"))) {
-
+            // Read the raw data line by line
             String line = br.readLine();
             while (line != null && !line.isEmpty()) {
 
+                // Split the current line by the space
                 String[] words = line.split("\\s+");
                 for (String word : words) {
                     if (!dict.contains(word)) {
+                        // If the word doesn't occur before, store it to the dictionary
                         dict.add(word);
                     }
                 }
+                // read the next line
                 line = br.readLine();
             }
 
             PrintWriter writer = new PrintWriter("dictionary.txt", "UTF-8");
+            // Navigate through the dictionary in a descending order
             for (Iterator<String> iter = dict.descendingIterator(); iter.hasNext(); ) {
                 String word = iter.next();
+                // write the word to the dictionary.txt
                 writer.println(word);
             }
             writer.close();
